e473def3a66b51ef0d276d80d0338137132deae9
Orisabiyi <orisabiyidavid@gmail.com>
2025-01-12 07:54:51 +0100

    feat: add expo-image package and update Home screen layout with placeholder image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
     </View>
   );
 }
@@ -19,14 +20,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 36,
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
