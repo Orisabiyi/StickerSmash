87b76d5ba641df9abc9f37fedfb2c0e80f600a21
Orisabiyi <orisabiyidavid@gmail.com>
2025-01-16 06:16:54 +0100

    feat: enhance image selection by integrating selected image display in ImageViewer and updating Button actions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
@@ -7,6 +8,10 @@ import Button from "@/components/Button";
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
   const pickImageAsync = async function () {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
@@ -14,17 +19,24 @@ export default function Index() {
       quality: 1,
     });
 
-    if (!result.canceled) console.log(result);
+    if (!result.canceled) setSelectedImage(result.assets[0].uri);
     else alert("You did not select an image");
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
